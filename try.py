8cb81580524f6b2a9938e95f208393906bc2d86d
napoleanBonaparte <106796606+napoleanBonaparte@users.noreply.github.com>
2022-06-04 12:06:48 +0530

    Add files via upload

---
+++ try.py
@@ -0,0 +1,16 @@
+yt_list = ["Pewdipie", "Wallibear", "Markiplier", "MrsOki", "MrBeats", "Belle Deplhine", "KSI", "Vikkstar123", "Manny",
+           "Zerkaa", "Pokimane", "BeastBoyShub", "Eminem", "YungFilly", "Mia Khalifa", "Technoblade", "HannahXXRose",
+           "NotAestheticHannah", "Javed", "TechnoGamers", "Nick Crowley", "Nexpo", "Disrupt", "COC India", "TSG Gamer"]
+
+x = input("Enter your first string")
+y = input("Enter your second string")
+z = input("Enter your third string")
+
+x = x[0]
+y = y[0]
+z = z[0]
+i = 0
+
+for i in range(len(yt_list)):
+    if x in yt_list[i] and y in yt_list[i] and z in yt_list[i]:
+        print(yt_list[i])
